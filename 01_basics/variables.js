f8aab3487b2ccfafd891c5f63be2e77d31a49ec5
basitsheikhabdul <147522316+basitsheikhabdul@users.noreply.github.com>
2025-02-20 18:37:55 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12311
+let accountEmail = "skabdulbasit@gmail.com"
+var city = "Jamshedpur"
+country = "India"
+// accountId = 1231
+accountEmail = "hello@gmail.com"
+city = "Rourkela"
+country = "Japan"
+
+/*
+Should Never use var as to declare a variable
+*/
+console.table([accountId, accountEmail, city, country]);
