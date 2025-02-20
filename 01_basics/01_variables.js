9caa58786dd948c2c08742e44f5bd8756f1b80d7
basitsheikhabdul <147522316+basitsheikhabdul@users.noreply.github.com>
2025-02-20 19:27:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12311 //const cant be changed
+let accountEmail = "skabdulbasit@gmail.com" //always use thsese keyword
+var city = "Jamshedpur"
+country = "India"
+// accountId = 1231
+accountEmail = "hello@gmail.com"
+city = "Rourkela"
+country = "Japan"
+let accountState;
+
+/*
+Should Never use var as to declare a variable
+*/
+console.table([accountId, accountEmail, city, country, accountState]);
