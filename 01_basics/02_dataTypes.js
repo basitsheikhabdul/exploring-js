8f747fd73790e379219067fa12bd90c4e53a32c4
basitsheikhabdul <147522316+basitsheikhabdul@users.noreply.github.com>
2025-02-20 19:29:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // This is used to treat all the code as newer version of js
+// alert(3+3) // Cant be used because of nodejs, it is not a browser
+
+console.log(3+
+    3);  // code readability is the higher priority
+
+let say = "Basit" // string dataTypes
+let age = 22
+
+// number => 
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value  //it is an object//
+// undefined=> value is not defined yet
+// symbol => unique 
+
+// object
+
+console.log(typeof true); //bool
+console.log(typeof age);  //number
+console.log(typeof say);  //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
